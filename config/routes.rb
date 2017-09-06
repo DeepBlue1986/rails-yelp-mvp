ed29d7887d8f29e9e409e562bb7985fb439f8171
Youssef Iraqui <youssef.iraqui@gmail.com>
2017-09-06 16:51:52 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,47 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get 'reviews/index'
+
+  get 'reviews/show'
+
+  get 'reviews/new'
+
+  get 'reviews/edit'
+
+  get 'reviews/update'
+
+  get 'reviews/destroy'
+
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
+  get 'restaurants/edit'
+
+  get 'restaurants/update'
+
+  get 'restaurants/destroy'
+
+  get 'reviews/new'
+  get 'reviews/create'
+
+  resources :restaurants do
+      resources :reviews, only: [:new, :create]
+      end
+  #   collection do
+  #     get 'top' , to: 'restaurants#top' # => restaurants/top
+  #   end
+
+  #   member do
+  #     get 'chef', to: 'restaurants#chef'
+  #   end
+
+  # namespace :admin do
+  #   resources :restaurants, only: [:index]
+  # end
+
+
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,35 @@
+
+hip = Restaurant.new(name: "hip fish", address: "Gracia north", category: "japanese")
+hip.save
+yummy = Review.new(content: "yummy yummy", rating: 4)
+yummy.restaurant = hip
+yummy.save
+
+empanada = Restaurant.new(name:"mafalda", address: "Joanic", category: "belgian")
+empanada.save
+great = Review.new(content: "amazing one!", rating: 4)
+great.restaurant = empanada
+great.save
+
+machina = Restaurant.new(name:"machina pasta", address: "gracia center", category: "italian")
+machina.save
+wow = Review.new(content: "amazing great one!", rating: 4)
+wow.restaurant = machina
+wow.save
+
+vegan = Restaurant.new(name:"vegan place", address: "gracia", category: "chinese")
+vegan.save
+healthy = Review.new(content: "amazing great one!", rating: 4)
+healthy.restaurant = vegan
+healthy.save
+
+mcd = Restaurant.new(name:"mcdonalds", address: "everywhere", category: "chinese")
+mcd.save
+bouh = Review.new(content: "really not good", rating: 2)
+bouh.restaurant = mcd
+bouh.save
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
